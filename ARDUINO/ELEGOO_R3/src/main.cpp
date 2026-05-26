2cdd5dba841bb7483193af7317b508584ae65d2a
MoinMau <143607016+MoinMau@users.noreply.github.com>
2026-05-26 21:55:18 +0200

    LED Test

--- ARDUINO/ELEGOO_R3/src/main.cpp
+++ ARDUINO/ELEGOO_R3/src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// CNC Shield V3 defaults (X axis)
+const uint8_t STEP_PIN = 2; // X.STEP
+const uint8_t DIR_PIN = 5;  // X.DIR
+const uint8_t EN_PIN = 8;   // Enable (active LOW)
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(EN_PIN, OUTPUT);
+
+  digitalWrite(EN_PIN, LOW); // enable drivers
+  digitalWrite(DIR_PIN, LOW);
+}
+
+void stepPulse(uint16_t steps, uint16_t stepDelayUs) {
+  for (uint16_t i = 0; i < steps; i++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(stepDelayUs);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(stepDelayUs);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  stepPulse(400, 600); // move one direction
+  delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(DIR_PIN, !digitalRead(DIR_PIN)); // reverse
+  stepPulse(400, 600);
+  delay(500);
 }
