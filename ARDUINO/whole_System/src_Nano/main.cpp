4a0b79000fe4795016cca890061863522d34197d
MoinMau <143607016+MoinMau@users.noreply.github.com>
2026-06-08 15:59:41 +0200

    Servo test

--- ARDUINO/whole_System/src_Nano/main.cpp
+++ ARDUINO/whole_System/src_Nano/main.cpp
@@ -7,6 +7,8 @@ const uint8_t SERVO_PIN = 10;
 Servo servo;
 
 void setup() {
+    Serial.begin(115200);
+    analogReference(INTERNAL);
   pinMode(LED_PIN, OUTPUT);
   servo.attach(SERVO_PIN);
   servo.write(90);  // Center position
@@ -15,19 +17,16 @@ void setup() {
 void loop() {
   // LED blink
   digitalWrite(LED_PIN, HIGH);
+  servo.write(0);  // Center position
+  Serial.println(analogRead(A0));  // Simulate some work
   delay(500);
   digitalWrite(LED_PIN, LOW);
+  servo.write(180);  // Center position
+  Serial.println(analogRead(A0));  // Simulate some work
   delay(500);
-  
-  // Servo sweep
-  for (int angle = 0; angle <= 180; angle += 5) {
-    servo.write(angle);
-    delay(50);
-  }
-  
-  for (int angle = 180; angle >= 0; angle -= 5) {
-    servo.write(angle);
-    delay(50);
-  }
+  digitalWrite(LED_PIN, LOW);
+  servo.write(180);  // Center position
+  Serial.println(analogRead(A0));  // Simulate some work
+  delay(500);
 }
 
